09d7371499e2e2d1763d98a93c694766d539c940
finaclemence15 <finaclemence15@gmail.com>
2019-08-23 18:13:12 +0200

    adding authot properties

--- src/app/quote1/quote1.component.ts
+++ src/app/quote1/quote1.component.ts
@@ -8,11 +8,11 @@ import { Quotes } from '../quotes'
 })
 export class Quote1Component implements OnInit {
   quote:Quotes [] = [
-    {id:1, name:' Great things never come from comfort zones.'},
-    {id:2, name:' Success doesn’t just find you. You have to go out and get it.'},
-    {id:2, name:' Little things make big days.'},
-    {id:2, name:'  It’s going to be hard, but hard does not mean impossible.'},
-    {id:2, name:' Don’t wait for opportunity. Create it.'},
+    {id:1, name:' Great things never come from comfort zones.', author:'James'},
+    {id:2, name:' Success doesn’t just find you. You have to go out and get it.' , author:'Valens'},
+    {id:2, name:' Little things make big days.' , author:'John'},
+    {id:2, name:'  It’s going to be hard, but hard does not mean impossible.' , author:'Ange'},
+    {id:2, name:' Don’t wait for opportunity. Create it.', author:'Jane'},
   ];
 
   constructor() { }
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,4 +1,5 @@
 export class Quotes {
     id: number;
     name: string;
+    author: string;
 }
