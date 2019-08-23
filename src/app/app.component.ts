94d19faf50932801aae911ba075d123c1580aec6
finaclemence15 <finaclemence15@gmail.com>
2019-08-23 17:08:42 +0200

    creating array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Quotes } from './quotes'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quotes-app';
+  quote:Quotes [] = [
+    {id:1, name:' Great things never come from comfort zones.'},
+    {id:2, name:' Success doesn’t just find you. You have to go out and get it.'},
+    {id:2, name:' Little things make big days.'},
+    {id:2, name:'  It’s going to be hard, but hard does not mean impossible.'},
+    {id:2, name:' Don’t wait for opportunity. Create it.'},
+  ];
 }
